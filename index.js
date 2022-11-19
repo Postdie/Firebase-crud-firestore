6ff563d884f2338219e33869ad96fa04ee23d2d2
Postdie <fernandezdiego845@gmail.com>
2022-11-18 18:30:02 -0600

    segundo commit Configuracion basica

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('works');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCBgeQ9v2nxBTPZv_mhA0K2W6Rv8mv9TCs",
+    authDomain: "fir-javascript-crud-21ac3.firebaseapp.com",
+    projectId: "fir-javascript-crud-21ac3",
+    storageBucket: "fir-javascript-crud-21ac3.appspot.com",
+    messagingSenderId: "289856883504",
+    appId: "1:289856883504:web:a121661c3ef4fda5bbd6f5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
