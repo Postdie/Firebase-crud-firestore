dc3345e3ed601da8bb7aa26c6f562c7d37e8674e
Postdie <fernandezdiego845@gmail.com>
2022-11-18 19:01:43 -0600

    tercer commit Guardado de Dato en firestore

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {saveTask} from './firebase.js'
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyCBgeQ9v2nxBTPZv_mhA0K2W6Rv8mv9TCs",
-    authDomain: "fir-javascript-crud-21ac3.firebaseapp.com",
-    projectId: "fir-javascript-crud-21ac3",
-    storageBucket: "fir-javascript-crud-21ac3.appspot.com",
-    messagingSenderId: "289856883504",
-    appId: "1:289856883504:web:a121661c3ef4fda5bbd6f5"
-};
+window.addEventListener('DOMContentLoaded', () => {
+    
+})
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const taskForm = document.getElementById('task-form')
 
+taskForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const title = taskForm['task-title']
+    const description = taskForm['task-description']
+
+    saveTask(title.value, description.value)
+
+    taskForm.reset
+})
